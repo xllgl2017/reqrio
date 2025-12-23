6a7566e385a1cbd0ab65da2a81714b1cafbeda3f
win11 <xinlanxi2017@gmail.com>
2025-12-23 13:55:53 +0800

    s

--- py/setup.py
+++ py/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='reqrio', version='0.0.4', packages=find_packages(),
-      package_data={'reqrio': ['*.dll']}, include_package_data=True )
+      package_data={'reqrio': ['*.dll']}, include_package_data=True)
 
 '''
 D:\projects\py\baidu\.venv\Scripts\python.exe -m pip install --upgrade twine
