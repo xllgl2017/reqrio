d77a33782998c61816a6ef27405fabb5c95534b4
win11 <xinlanxi2017@gmail.com>
2026-01-01 23:28:52 +0800

    release 0.0.9

--- py/setup.py
+++ py/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='reqrio', version='0.0.8', packages=find_packages(),
+setup(name='reqrio', version='0.0.9', packages=find_packages(),
       package_data={'reqrio': ['*.dll']}, include_package_data=True, entry_points={
         'pyinstaller40': ["hook-dirs=reqrio.hooks"]
     })
